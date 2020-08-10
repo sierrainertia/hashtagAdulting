974c558048e943bda5d2979396fc66a8cad3093d
Sierra MacDonald <sierrainertia@gmail.com>
2020-08-10 16:42:23 -0400

    Updates

---
+++ precache-manifest.6dbf5dc680c8e1ea4bd9af1c003fd31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e81ddf61b9601e1d8aa75727d05b12",
+    "url": "/sierraMacDonaldProjectFive/index.html"
+  },
+  {
+    "revision": "2026f5b9f8d569e4e36e",
+    "url": "/sierraMacDonaldProjectFive/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "50e2911471955352a75c",
+    "url": "/sierraMacDonaldProjectFive/static/js/2.e18dbbe1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sierraMacDonaldProjectFive/static/js/2.e18dbbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2026f5b9f8d569e4e36e",
+    "url": "/sierraMacDonaldProjectFive/static/js/main.21abc857.chunk.js"
+  },
+  {
+    "revision": "24f043eb418c3d5f1f42",
+    "url": "/sierraMacDonaldProjectFive/static/js/runtime-main.6ea03ba1.js"
+  }
+]);
