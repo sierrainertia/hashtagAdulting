49adf4fbaf55f430a26ae02253cde77450ae5dee
Sierra MacDonald <sierrainertia@gmail.com>
2020-08-29 00:28:54 -0400

    Updates

---
+++ precache-manifest.aa4667e0fab2606c1f43d4d7e1018696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25ff680fba98a95c94411de3c3816d5",
+    "url": "/hashtagAdulting/index.html"
+  },
+  {
+    "revision": "2a0be2237072f9cdd6de",
+    "url": "/hashtagAdulting/static/css/main.b125efc6.chunk.css"
+  },
+  {
+    "revision": "50e2911471955352a75c",
+    "url": "/hashtagAdulting/static/js/2.e18dbbe1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hashtagAdulting/static/js/2.e18dbbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0be2237072f9cdd6de",
+    "url": "/hashtagAdulting/static/js/main.b6010629.chunk.js"
+  },
+  {
+    "revision": "58aaef2907a7b6510427",
+    "url": "/hashtagAdulting/static/js/runtime-main.ccba01e3.js"
+  }
+]);
